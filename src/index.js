1763b8c90a6ea15f7fa5648c9339418605387680
BlisS <contacto@fixter.org>
2017-06-15 18:54:08 -0500

    ajax

--- src/index.js
+++ src/index.js
@@ -3,7 +3,24 @@ document.getElementById('saludo').innerHTML = "Kiubo?";
 $('#loader').fadeOut();
 
 
-iziToast.success({
-	title:"BlisS",
-	message: "Lo logramo!"
-});
+function pachurrado(){
+	iziToast.success({
+		title:"BlisS",
+		message: "Me pachurraste!"
+	});
+}
+
+function getBooks(){
+	$.get('https://erik.fixter.org/products/', function(respuesta){
+		// console.log(respuesta[1].name);
+		for(var i=0;i<respuesta.length;i++){
+			$('#lista').append('<li>'+respuesta[i].name+ '</li>');
+		}
+	});
+}
+
+
+
+
+
+
