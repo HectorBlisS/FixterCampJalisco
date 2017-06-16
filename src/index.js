3a95e1bfdb44636410e37f8b8bb6435daeee0849
Oswaldinho <oswalfut_96@hotmail.com>
2017-06-15 19:33:11 -0500

    firebase login :smile:

--- src/index.js
+++ src/index.js
@@ -3,7 +3,43 @@ document.getElementById('saludo').innerHTML = "Kiubo?";
 $('#loader').fadeOut();
 
 
-iziToast.success({
-	title:"BlisS",
-	message: "Lo logramo!"
-});
+function Success(){
+	iziToast.success({
+		title:"Oswaldinho",
+		message: "Tiene hambre"
+	});
+}
+function getBooks(){
+	$.get('https://erik.fixter.org/products/', function(respuesta){
+		for(i=0;i<respuesta.length;i++){
+			$('#lista').append('<li>' + respuesta[i].name + '</li>')
+		}
+	});
+}
+
+//Iniciamos con firebase
+
+function logIn(){
+
+	var provider = new firebase.auth.GoogleAuthProvider();
+
+	firebase.auth().signInWithPopup(provider).then(function(result) {
+	  // This gives you a Google Access Token. You can use it to access the Google API.
+	  var token = result.credential.accessToken;
+	  // The signed-in user info.
+	  var user = result.user;
+	  console.log('usuario:',user)
+	  $('#name').text(user.displayName)
+	  $('#img').attr('src', user.photoURL)
+	  // ...
+	}).catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  // The email of the user's account used.
+	  var email = error.email;
+	  // The firebase.auth.AuthCredential type that was used.
+	  var credential = error.credential;
+	  // ...
+	});
+}
